c0cd703a8e84cf7eaab7d847e9b9e30a61f7b024
stefaleon <stefaleon@gmail.com>
2022-06-28 14:01:55 +0300

    Simulate Actions Using the Zombie Headless Browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -64,6 +64,9 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://localhost:3000/';
+const browser = new Browser();
+
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
